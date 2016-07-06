e0e2b0c09fcb8dac9fc2c8db138861d94d4bae27
mac <mac@macdemacbook-pro-97.local>
2016-07-06 17:18:24 +0800

    创键工程

---
+++ MyYiChat/MyYiChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyYiChat
+//
+//  Created by mac on 16/7/6.
+//  Copyright © 2016年 Earl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
